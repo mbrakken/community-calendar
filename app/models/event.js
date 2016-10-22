d76252a8139914068955856bc4c698c38f1dff33
Marc Brakken <marc.brakken@gmail.com>
2016-10-22 11:43:16 -0500

    create event model

---
+++ app/models/event.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  category: DS.attr('string'),
+  citySponsored: DS.attr('boolean'),
+  contact: DS.attr('string'),
+  cost: DS.attr(),
+  description: DS.attr(),
+  district: DS.attr(),
+  endDate: DS.attr('date'),
+  eventId: DS.attr('number'),
+  eventTime: DS.attr(),
+  location: DS.attr(),
+  startDate: DS.attr('date'),
+  title: DS.attr()
+});
